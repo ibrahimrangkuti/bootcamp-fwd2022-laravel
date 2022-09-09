0ed47fdf8f15f7ffec1a7ad4845c76dff421c842
ibrahimrangkuti <rangkutyibrahim765@gmail.com>
2022-09-09 11:30:19 +0700

    add jetstream & migrate

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +11,18 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js("resources/js/app.js", "public/js").postCss(
+    "resources/css/app.css",
+    "public/css",
+    [require("postcss-import"), require("tailwindcss")]
+);
+
+if (mix.inProduction()) {
+    mix.version();
+}
+
+mix.webpackConfig({
+    stats: {
+        children: true,
+    },
+});
